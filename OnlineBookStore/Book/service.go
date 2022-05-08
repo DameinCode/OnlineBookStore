09df20b0a30bb0080b1e2928651d9148ef3d370c
DameinCode <78813823+DameinCode@users.noreply.github.com>
2022-05-08 21:37:01 +0600

    Endterm Project

---
+++ OnlineBookStore/Book/service.go
@@ -0,0 +1,28 @@
+package book
+
+import (
+	"context"
+
+	bookrpc "github.com/DameinCode/Golang-spring-2022/Uni/Final/OnlineBookStore/Book/rpc"
+
+	"github.com/DameinCode/Golang-spring-2022/Uni/Final/OnlineBookStore/store"
+)
+
+// Service holds RPC handlers for the product service. It implements the product.ServiceServer interface.
+type service struct {
+	bookrpc.UnimplementedServiceServer
+	s store.BookStore
+}
+
+func NewService(s store.BookStore) *service {
+	return &service{s: s}
+}
+
+// Fetch all existing products in the system.
+func (s *service) ListBooks(ctx context.Context, r *bookrpc.ListBooksRequest) (*bookrpc.ListBooksResponse, error) {
+	return &bookrpc.ListBooksResponse{Books: s.s.Books()}, nil
+}
+func (s *service) BookOfId(ctx context.Context, r *bookrpc.BookOfIdRequest) (*bookrpc.BookOfIdResponse, error) {
+	boo, err := s.s.Book(r.Id)
+	return &bookrpc.BookOfIdResponse{Book: boo}, err
+}
